1ebb69648753f7583b39d7f8ce97dfa7881cef02
Damon P. Cortesi <d.lifehacker@gmail.com>
2020-03-29 16:36:44 -0700

    Kind of a good stopping point - can query a datasette endpoint and fetch the table and field names.

--- src/metabase/driver/datasette.clj
+++ src/metabase/driver/datasette.clj
@@ -13,19 +13,15 @@
 (defmethod driver/can-connect? :datasette [_ _]
     true)
 
-;;; ------------------------------------------- Utility functions -----------------------------------
-(defn- get-datasette-endpoint-url
-  "Returns the Datasette Endpoint URL from the config."
-  []
-  (:datasette_endpoint (:details (qp.store/database))))
+;;; ------------------------------------------- describe-database -----------------------------------
 
 (defn make-request
   "Make the HTTP request to the Datasette endpoint and return the response.
       If the response is 200 with a 'Continue wait' error message, try again."
   [resource query database]
-  (let [url        (resource)]
+  (let [url        (str resource)]
       (loop []
-      (log/debug "Request:" url query)
+      (log/info "Request:" url query)
       (let [resp (client/request {:method            :get
                                   :url               url
                                   :query-params      {"query" query}
@@ -45,14 +41,30 @@
           500 (throw (Exception. (format "Internal server error: %s" body)))
           :else (throw (Exception. (format "Unknown error. Body: %s" body))))))))
 
-(defn- get-endpoint
+(defn- get-endpoint-data
   "Get a datasette definition from an endpoint URL."
-  [database]
-  (let [resp   (make-request (get-datasette-endpoint-url) nil database)
+  [{{:keys [datasette_endpoint]} :details, :as database}]
+  (let [resp   (make-request datasette_endpoint nil database)
         body   (:body resp)]
     body))
 
 ; Read the datasette endpoint and pull out the table and schema
 (defmethod driver/describe-database :datasette [_ database]
-  (let [datasette-def (get-endpoint database)]
-  {:tables (set ({:name (:table datasette-def)}))}))
+  (let [datasette-def (get-endpoint-data database)]
+  {:tables #{{:name (:table datasette-def)
+              :schema nil
+              :description (:human_description_en datasette-def)}}}))
+
+;;; ----------------------------- describe-table -------------------------------
+(defmethod driver/describe-table :datasette
+  [_ database table]
+  {:name   (:name table)
+   :schema (:schema table)
+   :fields (set (for [field (:columns (get-endpoint-data database))]
+                  {:name field
+                   :base-type :type/Text
+                   :database-type "some.Random.String"}))}) ; Is database-type the raw type from the DB?
+
+;;; --------------------------------- query execution ------------------------------------------------------------
+(defmethod driver/mbql->native :datasette [_ query]
+  (log/info "MBQL:" query))
